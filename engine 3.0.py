3d84471313d0bb3968cc3494abd9f140f2aebad9
lex-zhukov <133701540+lex-zhukov@users.noreply.github.com>
2023-08-27 18:31:28 +0300

    Add files via upload

--- engine 3.0.py
+++ engine 3.0.py
@@ -282,8 +282,8 @@ def pan_center(area):
             target = {'y':focus['y'], 'x':minx}
             sweet_spot = {'y':focus['y'], 'x':(minx + distance)}
             midpoint_dot = {'y':focus['y'], 'x':(minx + distance - midpoint)}
-        area_34 = area_search(v_areas, 0)
-        area_56 = area_search(v_areas, 1)
+        area_34 = area_search(h_areas, 0)
+        area_56 = area_search(h_areas, 1)
         positions = c_3456('y')
         pan_1 = {'y':(focus['y'] - (0.5 * l)), 'x':target['x']}
         pan_2 = {'y':(focus['y'] + (0.5 * l)), 'x':target['x']}
@@ -588,8 +588,8 @@ def get_results(variant):
     for point in location_AS:
         variant.insert(0, point)
 
-points = [{'x': 0.0, 'y': 0.0}, {'x': 0.0, 'y': 3.0}, {'x': 0.5, 'y': 3.0}, {'x': 0.5, 'y': 4.5}, {'x': 1.5, 'y': 4.5}, {'x': 1.5, 'y': 1.0}, {'x': 2.0, 'y': 1.0}, {'x': 2.0, 'y': 6.0}, {'x': 5.0, 'y': 6.0}, {'x': 5.0, 'y': 4.5}, {'x': 6.0, 'y': 4.5}, {'x': 6.0, 'y': 2.5}, {'x': 4.0, 'y': 2.5}, {'x': 4.0, 'y': 0.0}]
-#points = [{'x': 0.0, 'y': 0.0}, {'x': 0.0, 'y': 3.0}, {'x': 3.0, 'y': 3.0}, {'x': 3.0, 'y': 1.0}, {'x': 4.0, 'y': 1.0}, {'x': 4.0, 'y': 3.0}, {'x': 5.0, 'y': 3.0}, {'x': 5.0, 'y': 0.0}]
+#points = [{'x': 0.0, 'y': 0.0}, {'x': 0.0, 'y': 3.0}, {'x': 0.5, 'y': 3.0}, {'x': 0.5, 'y': 4.5}, {'x': 1.5, 'y': 4.5}, {'x': 1.5, 'y': 1.0}, {'x': 2.0, 'y': 1.0}, {'x': 2.0, 'y': 6.0}, {'x': 5.0, 'y': 6.0}, {'x': 5.0, 'y': 4.5}, {'x': 6.0, 'y': 4.5}, {'x': 6.0, 'y': 2.5}, {'x': 4.0, 'y': 2.5}, {'x': 4.0, 'y': 0.0}]
+points = [{'x': 0.0, 'y': 0.0}, {'x': 0.0, 'y': 3.0}, {'x': 3.0, 'y': 3.0}, {'x': 3.0, 'y': 1.0}, {'x': 4.0, 'y': 1.0}, {'x': 4.0, 'y': 3.0}, {'x': 5.0, 'y': 3.0}, {'x': 5.0, 'y': 0.0}]
 check_points = [] # список точек для проверок
 vertical_walls = [] # список вертикальных стен
 horizontal_walls = [] # список горизонтальных стен
@@ -921,12 +921,14 @@ for variant in variants: # вывод для отладки
 from svg_turtle import SvgTurtle
 
 def draw_room(t, variant):
-    def draw_element(t, variant, color, position, radius):
+    def draw_element(t, variant, color, position, radius, text):
         t.fillcolor(color)
-        t.pencolor(color)
+        t.pencolor('white')
         t.begin_fill()
         t.penup()
+        t.penup()
         t.goto(((variant[position]['x'] - move_x) * 50), (((variant[position]['y'] - move_y) * 50) - (radius)))
+        t.write(text, align = 'center', font = ("Verdana", 9, "normal"))
         t.pendown()
         if radius == 10:
             t.fd(10)
@@ -949,6 +951,7 @@ def draw_room(t, variant):
     move_x = max(all_x) / 2
     move_y = max(all_y) / 2
     t.fillcolor('skyblue')
+    t.pensize(0)
     t.begin_fill()
     t.penup()
     t.goto(((0 - move_x) * 50), ((0 - move_y) * 50))
@@ -959,13 +962,13 @@ def draw_room(t, variant):
     t.end_fill()
     # здесь рисуем точки АС
         # первая
-    draw_element(t, variant, 'blue', 0, 5)
+    draw_element(t, variant, 'blue', 0, 5, '')
         # вторая
-    draw_element(t, variant, 'blue', 1, 5)
+    draw_element(t, variant, 'blue', 1, 5, '')
     # ss
-    draw_element(t, variant, 'green', 3, 5)
+    draw_element(t, variant, 'green', 3, 5, '')
     for pos in variant[5:]:
-        draw_element(t, variant, 'red', variant.index(pos), 10) 
+        draw_element(t, variant, 'black', variant.index(pos), 10, (str(variant.index(pos) - 4))) 
     t.hideturtle()
 
 def write_file(draw_func, filename, width, height):
